55837b8ca7f5dd6fbf4f54e2642c605286f3a3ce
jiukeem <jeewoo0521@naver.com>
2021-08-13 17:09:19 +0900

    [apply] set main view table with recyclerview

--- app/src/main/java/com/example/goaltracker/MainActivity.kt
+++ app/src/main/java/com/example/goaltracker/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.goaltracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.goaltracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.recyclerView?.adapter = CalendarAdapter(this)
+        // 어댑터 설정은 여기서 해도 되고 xml에서 해도된다.
+        // viewModel 에서 adapter = CalendarAdapter() 변수 하나 만들어놓고 binding?.calendarRecycler?.adapter = viewModel?.adapter 이렇게 해도 되고
+        // 방법은 다양하다.
     }
 }
