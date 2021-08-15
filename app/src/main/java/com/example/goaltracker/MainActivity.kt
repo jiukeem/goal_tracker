74512ebfbfc1fd5ce9137ba281e14f7d34a50da0
jiukeem <jeewoo0521@naver.com>
2021-08-15 21:53:36 +0900

    [apply] image removal feature

--- app/src/main/java/com/example/goaltracker/MainActivity.kt
+++ app/src/main/java/com/example/goaltracker/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.goaltracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
 import com.example.goaltracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding?.recyclerView?.adapter = CalendarAdapter(this)
+        binding?.recyclerView?.adapter = TrackerTableAdapter(this)
         // 어댑터 설정은 여기서 해도 되고 xml에서 해도된다.
         // viewModel 에서 adapter = CalendarAdapter() 변수 하나 만들어놓고 binding?.calendarRecycler?.adapter = viewModel?.adapter 이렇게 해도 되고
         // 방법은 다양하다.
--- app/src/main/java/com/example/goaltracker/TrackerTableAdapter.kt
+++ app/src/main/java/com/example/goaltracker/TrackerTableAdapter.kt
@@ -1,10 +1,8 @@
 package com.example.goaltracker
 
 import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
-import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +10,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.Logger
 
-class CalendarAdapter(private val context: Context): RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+class TrackerTableAdapter(private val context: Context): RecyclerView.Adapter<TrackerTableAdapter.CalendarViewHolder>() {
+    private val itemList = mutableListOf<Cell>().apply {
+        for (i in 1..35) {
+            this.add(Cell())
+        }
+    }
+
     // 이 레이아웃에 연결할거에요. 뷰홀더에게 어떤 레이아웃인지 인플레이트해서 넘겨주는 역할. (inflate 란 레이아웃을 코드에서 사용할 수 있도록 객체화해주는 과정이다.)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_date, parent, false)
@@ -24,13 +26,13 @@ class CalendarAdapter(private val context: Context): RecyclerView.Adapter<Calend
 
     // 전체 갯수는 이래요. 전체 아이템이 몇개나 되는지 계산.
     override fun getItemCount(): Int {
-        return 35
+        return itemList.size
     }
 
     // n번째 아이템 안의 내용은 이렇게 설정할게요. 변수의 속성을 지정해주는 역할(visibility, text 등)
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
         holder.imageView.apply {
-            setOnClickListener { showImageSelectionPopUp(this) } // 이미지가 있는 경우는 반대로 없애야하니 viewmodel 로 콜백을 보내서 거기서 처리하자
+            setOnClickListener { onItemClicked(itemList[position], this) } // 이미지가 있는 경우는 반대로 없애야하니 viewmodel 로 콜백을 보내서 거기서 처리하자
         }
         if (position >= 28) {
             holder.lineForBottom.setBackgroundColor(ContextCompat.getColor(context, R.color.black))
@@ -43,12 +45,35 @@ class CalendarAdapter(private val context: Context): RecyclerView.Adapter<Calend
         val lineForBottom: TextView = view.findViewById(R.id.txtForBottom)
     }
 
-    private fun showImageSelectionPopUp(imageView: ImageView) {
+    private fun onItemClicked(item: Cell, imageView: ImageView) {
+        if (item.isImageInserted) {
+            showImageRemovePopUp(item, imageView)
+        } else {
+            showImageSelectionPopUp(item, imageView)
+        }
+    }
+
+    private fun showImageSelectionPopUp(item: Cell, imageView: ImageView) {
         AlertDialog.Builder(context)
                 .setMessage("insert image?")
                 .setPositiveButton("Insert", DialogInterface.OnClickListener { dialog, _ ->
                     dialog.dismiss()
+                    item.isImageInserted = true
                     imageView.setImageResource(R.mipmap.tracker_icon_done_foreground)
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, _ ->
+                    dialog.dismiss()
+                })
+                .show()
+    }
+
+    private fun showImageRemovePopUp(item: Cell, imageView: ImageView) {
+        AlertDialog.Builder(context)
+                .setMessage("remove image?")
+                .setPositiveButton("Remove", DialogInterface.OnClickListener { dialog, _ ->
+                    dialog.dismiss()
+                    item.isImageInserted = false
+                    imageView.setImageResource(0)
                 })
                 .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, _ ->
                     dialog.dismiss()
